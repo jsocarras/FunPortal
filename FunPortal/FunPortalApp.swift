4fbdbba8d8697142606fb2129a9a456a5d7c0e6f
JRS <jrysocarras@gmail.com>
2024-12-06 12:50:18 -0600

    Initial Commit

---
+++ FunPortal/FunPortalApp.swift
@@ -0,0 +1,17 @@
+//
+//  FunPortalApp.swift
+//  FunPortal
+//
+//  Created by Joseph Socarras on 12/6/24.
+//
+
+import SwiftUI
+
+@main
+struct FunPortalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FunPortalTests/FunPortalTests.swift
@@ -0,0 +1,17 @@
+//
+//  FunPortalTests.swift
+//  FunPortalTests
+//
+//  Created by Joseph Socarras on 12/6/24.
+//
+
+import Testing
+@testable import FunPortal
+
+struct FunPortalTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
